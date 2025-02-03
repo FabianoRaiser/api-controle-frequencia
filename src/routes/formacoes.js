ece240376aeab97f226f098ede23686fdeefb9cd
Fabiano Raiser <fabiano.raiser16@gmail.com>
2025-02-03 19:11:33 -0300

    refactor: :recycle: Ajustes para o front das formações

--- src/routes/formacoes.js
+++ src/routes/formacoes.js
@@ -6,12 +6,22 @@ const router = express.Router();
 // Buscar todas as formações
 router.get('/', async (req, res) => {
     try {
+        console.log("Tentadno buscar formações...");
         const { data, error } = await supabase
             .from('formacoes')
-            .select('*');
+            .select(`
+                formacao_id,
+                nome_formacao,
+                carga_horaria,
+                data,
+                local,
+                modalidade,
+                periodo
+                `);
 
         if (error) throw error;
-
+        
+        console.log("Formações encotradas:", data?.length || 0);
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -23,8 +33,37 @@ router.get('/:id', async (req, res) => {
     try {
         const { data, error } = await supabase
             .from('formacoes')
-            .select('*')
-            .eq('id', req.params.id)
+            .select(`
+                formacao_id,
+                turmas (
+                    nome_turma,
+                    etapa
+                ),
+                nome_formacao,
+                descricao,
+                carga_horaria,
+                local,
+                data,
+                created_at,
+                created_by (
+                    nome
+                ),
+                modified_at,
+                modified_by (
+                    nome
+                ),
+                periodo,
+                modalidade,
+                presencas (
+                    presenca_id,
+                    professores (
+                        nome_professor,
+                        matricula
+                    ),
+                    created_at
+                )
+                `)
+            .eq('formacao_id', req.params.id)
             .single();
 
         if (error) throw error;
